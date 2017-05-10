fcc7ed5653548b206bce4874168f4747ab915396
Stef5568 <mamulea.55@gmail.com>
2017-05-10 22:39:20 +0300

    Site-ul vechi al lui Marius

---
+++ js/MariusArt.js
@@ -0,0 +1,15 @@
+if (typeof jQuery === "undefined") {
+  console.log("jQuery not installed");
+} else {
+  console.log("jQuery installed correctly");
+}
+
+$("div").click(function () {
+
+  alert("Clicked");
+
+});
+
+
+var tablou = [];
+console.log("A trecut!");
